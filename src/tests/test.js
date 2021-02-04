b72f67d1eded57be1d99edb00a7338f514c2035a
Nikita Klyuev <52157662+nvk97@users.noreply.github.com>
2021-02-04 21:00:58 +0300

    Test (#3)
    * add simple test

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,9 +1,18 @@
 import { mount } from '@vue/test-utils'
 import vTrafficLight from '@/components/v-traffic-light.vue'
 
-const wrapper = mount(vTrafficLight,{
-    propsData:{
-        routeContext:'blue'
-    }
+
+test('check props and data',()=>{// Микро тест компонента
+    const wrapper = mount(vTrafficLight,{
+        propsData:{
+            routeContext:'blue',
+            totalTicks:20,
+            toRedirect:'red'
+        }
+    })
+    expect(wrapper.vm.redirectTo).toBe('red')
+    expect(wrapper.vm.ticksRemain).toBe(20)
 })
+
+   
 
